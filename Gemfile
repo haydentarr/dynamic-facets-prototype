4a6367b2a40d2ffc35cefc689dc417ddf74f16ca
Hayden Tarr <hayden@haydentarr.com>
2015-07-08 17:34:24 -0700

    fixed auth

--- Gemfile
+++ Gemfile
@@ -39,7 +39,7 @@ gem 'httparty'
 gem 'upwork-api'
 gem 'pry'
 
-gem 'omniauth-upwork', path: '/Users/Hayden/Dropbox/Public/OdeskApi-AngularOdesk/omniauth-upwork-1.0.0'
+gem 'omniauth-upwork', git: 'https://github.com/haydentarr/upwork-omni.git'
 
 gem 'figaro'
 
--- app/controllers/ats_controller.rb
+++ app/controllers/ats_controller.rb
@@ -7,9 +7,7 @@ class AtsController < ApplicationController
     if $auth.present?
       @data ||= $auth.get('http://match.prod.svc.odesk.com/search/b/v1/profiles?q='+params[:query].to_s+'&predicted_tier='+params[:exp].to_s+'&dynamic_facets=true&paging=0;100&dynamic_facets_src=all_facets').read_body
       #@data ||= $auth.get('/api/profiles/v2/search/providers.json?q='+params[:query].to_s+'&dynamic_facets=true&paging=0;100&dynamic_facets_src=all_facets').read_body
-
       #@data ||= $auth.get('/api/profiles/v2/search/providers.json?dynamic_facets=true&q=php&profile_access=public,auth&preserve_facet=all').read_body
-
     end
     if @data.present?
       @results = @data
@@ -17,11 +15,5 @@ class AtsController < ApplicationController
     else
       respond_with(User.all)
     end
-  end
-
-
-  def get_hash(data)
-    @hash_data = JSON.parse(data)
-    return @hash_data
   end
 end
